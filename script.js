dd0b9dbf534256a295ab07acc605ebebfe64fe32
Nico <nlfernandez3@gmail.com>
2023-03-28 18:47:10 -0300

    Proyecto finalizado

--- script.js
+++ script.js
@@ -10,21 +10,39 @@ function playRound(playerSelection, computerSelection) {
     let result = "";
     
     if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
-        result = `You Win! ${playerSelection} beats ${computerSelection}`;
-        playerScore += 1;
+        if (playerScore === 5 || computerScore === 5) {
+            return;
+        } else if (playerScore < 4) {
+            result = `You Win! ${playerSelection} beats ${computerSelection}`;
+            playerScore += 1;
+        } else if (playerScore === 4) {
+            result = "You Won the Game!";
+            playerScore += 1;
+        }
     } else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock") {
-        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
-        computerScore += 1;
-    } else {
-        result = "Tie!";
+        if (playerScore === 5 || computerScore === 5) {
+            return;
+        } else if (computerScore < 4) {
+            result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            computerScore += 1;
+        } else if (computerScore === 4) {
+            result = "You Lost the Game!"
+            computerScore += 1;
+        }
+    } else if (playerSelection === computerSelection) {
+        if (playerScore === 5 || computerScore === 5) {
+            return;
+        } else if (playerScore < 5 && computerScore < 5) {
+            result = "Tie!";
+        }
     }
-    div.textContent = `${result}. Player: ${playerScore}, Computer: ${computerScore}`;
+    results.textContent = `${result}. Player: ${playerScore} - Computer: ${computerScore}`;
 }
 
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
-const div = document.querySelector('div');
+const results = document.querySelector('.results');
 
 
 rock.addEventListener('click', function() {
